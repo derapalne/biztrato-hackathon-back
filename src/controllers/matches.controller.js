396db8d58ad594c5e69ce2e9a03810a067dd7440
derapalne <derapalne@gmail.com>
2022-11-17 00:24:56 -0300

    Minor fix in matches.controller.js + .gitignore

--- src/controllers/matches.controller.js
+++ src/controllers/matches.controller.js
@@ -2,21 +2,14 @@ import { getMatchesService } from "../service";
 
 const getMatches = async (req, res) => {
     try {
-        const matches = await getMatchesService();
-        const sortedGroups = {
-            GROUP_A: [],
-            GROUP_B: [],
-            GROUP_C: [],
-            GROUP_D: [],
-            GROUP_E: [],
-            GROUP_F: [],
-            GROUP_G: [],
-            GROUP_H: [],
-        };
-        console.log(matches.resultSet.count);
-        for (let m = 0; m < matches.resultSet.count; m++) {
-            if (!matches.matches[m].group) continue;
-            sortedGroups[matches.matches[m].group].push(matches.matches[m]);
+        const data = await getMatchesService();
+        const sortedGroups = {};
+        for (let m = 0; m < data.resultSet.count; m++) {
+            if (!data.matches[m].group) continue;
+            if (!sortedGroups[data.matches[m].group]) {
+                sortedGroups[data.matches[m].group] = [];
+            }
+            sortedGroups[data.matches[m].group].push(data.matches[m]);
         }
         res.status(200).json(sortedGroups);
     } catch (error) {
